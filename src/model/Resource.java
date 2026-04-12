d1a54fffa014014f36c80f6e45e2207ae39827ed
CaptainGeorgia <elvinnn2008@gmail.com>
2026-04-12 17:20:29 +0400

    Create Resource.java

---
+++ src/model/Resource.java
@@ -0,0 +1,11 @@
+package model;
+
+public enum Resource {
+    JET_FUEL("Jet Fuel", "L"),
+    MEALS("In-flight Meals", "units");
+
+    ### This part will be done later...
+
+    @Override
+    public String toString() { return displayName; }
+}
