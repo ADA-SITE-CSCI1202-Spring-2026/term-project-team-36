49ef3532855643c6eca72568f99b5f42dae44736
CaptainGeorgia <elvinnn2008@gmail.com>
2026-04-26 22:43:40 +0400

    Update Resource.java

--- src/model/Resource.java
+++ src/model/Resource.java
@@ -4,7 +4,16 @@ public enum Resource {
     JET_FUEL("Jet Fuel", "L"),
     MEALS("In-flight Meals", "units");
 
-    ### This part will be done later...
+    private final String displayName;
+    private final String unit;
+
+    Resource(String displayName, String unit) {
+        this.displayName = displayName;
+        this.unit = unit;
+    }
+
+    public String getDisplayName() { return displayName; }
+    public String getUnit() { return unit; }
 
     @Override
     public String toString() { return displayName; }
