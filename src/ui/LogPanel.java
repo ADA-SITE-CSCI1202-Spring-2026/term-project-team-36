214c292dddd2a4e236e4412a1b3e73234311ff37
ShMansee <amanbekabilmansur@gmail.com>
2026-04-28 23:30:11 +0400

    Update LogPanel.java

--- src/ui/LogPanel.java
+++ src/ui/LogPanel.java
@@ -16,19 +16,22 @@ public class LogPanel extends VBox {
     public LogPanel() {
         super(8);
         setPadding(new Insets(12));
-       
+        setStyle("-fx-background-color: #0d0d0d; -fx-border-color: #2a5c2a; -fx-border-width: 2;");
+
         Label title = new Label(">> DISPATCH RADIO");
         title.setFont(Font.font("Monospaced", FontWeight.BOLD, 13));
-   
+        title.setTextFill(Color.web("#39ff14"));
 
         logArea = new TextArea();
         logArea.setEditable(false);
         logArea.setWrapText(true);
-      
+        logArea.setPrefHeight(180);
+        logArea.setStyle(
             "-fx-control-inner-background: #0a0a0a;" +
-         
+            "-fx-text-fill: #39ff14;" +
             "-fx-font-family: 'Monospaced';" +
-         
+            "-fx-font-size: 11px;" +
+            "-fx-border-color: #1a3a1a;"
         );
 
         getChildren().addAll(title, logArea);
